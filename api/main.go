ec3c0e9c5c7994b187de9ac1be4b7a1e0bb669d0
Alan Enrique Maldonado Navarro <mlgalanmn88@gmail.com>
2020-03-23 20:21:06 -0600

    FEATURE: Added Middleware

--- api/main.go
+++ api/main.go
@@ -7,14 +7,16 @@ import (
 	"golang-distributed-parallel-image-processing/api/upload"
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 )
 
 //Module ...
 type Module struct {
-	Method   string
-	Path     string
-	Function echo.HandlerFunc
+	Method     string
+	Path       string
+	Function   echo.HandlerFunc
+	Middleware *echo.MiddlewareFunc
 }
 
 type Message struct {
@@ -32,26 +34,36 @@ func LoadModules() []*Module {
 		&Module{
 			Method:   "POST",
 			Path:     "/login",
-			Function: login.LoginResponse, //TODO Add a function response for login
+			Function: login.LoginResponse,
 		},
 		&Module{
-			Method:   "POST",
-			Path:     "/logout",
-			Function: logout.LogoutResponse, //TODO Add a function response for logout
+			Method:     "POST",
+			Path:       "/logout",
+			Function:   logout.LogoutResponse, //TODO Add a function response for logout
+			Middleware: &IsLoggedIn,
 		},
 		&Module{
-			Method:   "GET",
-			Path:     "/status",
-			Function: status.StatusResponse, //TODO Add a function response for status
+			Method:     "GET",
+			Path:       "/status",
+			Function:   status.StatusResponse, //TODO Add a function response for status
+			Middleware: &IsLoggedIn,
 		},
 		&Module{
-			Method:   "POST",
-			Path:     "/upload",
-			Function: upload.UploadResponse, //TODO Add a function response for upload
+			Method:     "POST",
+			Path:       "/upload",
+			Function:   upload.UploadResponse, //TODO Add a function response for upload
+			Middleware: &IsLoggedIn,
 		},
 	}
 }
 
 func rootResponse(c echo.Context) error {
 	return c.JSON(http.StatusForbidden, &Message{Message: "You're not allowed to do this. [ERR WRONG METHOD]"})
+}
+
+func private(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.String(http.StatusOK, "Welcome "+name+"!")
 }
--- api/status/status.go
+++ api/status/status.go
@@ -3,16 +3,9 @@ package status
 import (
 	"net/http"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 )
 
 func StatusResponse(c echo.Context) error {
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(jwt.MapClaims)
-	name := claims["name"].(string)
-	return c.JSON(http.StatusOK, &Message{
-			Message:"Welcome" + name + "!",
-		}
-	)
+	return c.String(http.StatusOK, "Status: bruh")
 }
