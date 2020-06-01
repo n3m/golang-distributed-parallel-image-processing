4b52de16997966d1067a3314fbfdbe49639a2b9f
Alan Enrique Maldonado Navarro <mlgalanmn88@gmail.com>
2020-06-01 18:06:53 -0500

    .

--- worker/main.go
+++ worker/main.go
@@ -138,6 +138,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
 	pb.RegisterControllerServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
